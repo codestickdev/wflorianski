820cdce2615ad17a568f514dcbf139d9a2b28556
codestickdev <piotrdevv@gmail.com>
2023-01-10 20:36:47 +0100

    Update page_cookies.php

--- page_templates/page_cookies.php
+++ page_templates/page_cookies.php
@@ -12,7 +12,7 @@ get_header(); ?>
         <div class="aboutSidebar">
             <h3><?php pll_e('Czujesz to samo?') ?></h3>
             <p><?php pll_e('Skontaktuj się ze mną i obserwuj') ?></p>
-            <a href="#" class="instagram">
+            <a href="https://www.instagram.com/wojciech.florianski/" target="_blank" class="instagram">
                 <img src="<?php echo get_template_directory_uri() . '/images/icons/ig_ico.svg'; ?>"/>
                 <span>@Wojciech.Florianski</span>
             </a>
