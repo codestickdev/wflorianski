169bee54d2f268138a675ca509195f4c0de8af04
codestickdev <piotrdevv@gmail.com>
2023-01-10 20:31:43 +0100

    test

--- functions.php
+++ functions.php
@@ -199,6 +199,20 @@ add_action('init', function() {
 	pll_register_string('wflorainski', 'Inne posty, które mogą Ci się spodobać');
 });
 
+/* ACF local JSON */
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+function my_acf_json_save_point( $path ) {
+    $path = get_stylesheet_directory() . '/acf-json';
+    return $path;
+}
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+function my_acf_json_load_point( $paths ) {
+    unset($paths[0]);
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+}
+
 /**
  * Register ACF Blocks
  */
